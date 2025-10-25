69ef61efd51ca016280731efa6b3a1d398aa4c21
agrahafiz13 <agrahafiz2@gmail.com>
2025-10-25 13:26:39 +0700

    merubnah container pertama menjadi sesuai contoh

--- lib/main.dart
+++ lib/main.dart
@@ -30,19 +30,89 @@ class MyHome extends StatelessWidget {
           Icon(Icons.person, color: Colors.white),
           SizedBox(width: 10),
           Icon(Icons.account_tree),
+          SizedBox(width: 10),
           Icon(Icons.ac_unit),
           SizedBox(width: 20),
         ],
       ),
-      body: Container(
-        alignment: Alignment.center,
-        margin: EdgeInsets.only(top: 20),
+      body: Column(
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.all(15),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 0, 195, 255), // biru muda
+                  Color.fromARGB(255, 255, 238, 0), // kuning
+                ],
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Kamen",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Rider",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Zio",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
 
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Text("Ini adalah teks di dalam container"),
+          Container(
+            width: 150,
+            height: 150,
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Colors.lightBlueAccent,
+              gradient: const LinearGradient(
+                colors: [
+                  Color(0xFF00B4DB), // biru muda
+                  Color(0xFFFFE000), // kuning cerah
+                ],
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+              ),
+            ),
+
+            child: Text(
+              "Hello World..!!",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
